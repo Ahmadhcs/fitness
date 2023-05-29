bbfd2efad99995861c3c41cc73a0b70bac628e95
Ahmadhcs <aalmesned1@gmail.com>
2023-05-29 21:42:44 +0300

    Added dependcies

---
+++ Screens/HomeScreen.js
@@ -0,0 +1,23 @@
+import { View, Text } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const HomeScreen = () => {
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false,
+        });
+    }, [])
+
+  return (
+    <SafeAreaView>
+      <Text>HomeScrenj jn en</Text>
+    </SafeAreaView>
+
+  )
+}
+
+export default HomeScreen
