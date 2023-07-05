77a86696733ae5ebab44c6fb715e498b9c55c08d
Mustafa <mustafataibah@gmail.com>
2023-07-04 22:12:15 -0400

    Workout page with dynamic box add functionality, still need to work on the other functional aspects just wanted to save my work while I expreriment

--- screens/Workout.js
+++ screens/Workout.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -7,10 +6,13 @@ import {
   Dimensions,
   StyleSheet,
   TouchableOpacity,
+  Modal,
+  ScrollView,
+  TouchableWithoutFeedback,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Ionicons } from "@expo/vector-icons";
-import { style } from "d3";
+import { Feather } from "@expo/vector-icons";
+import { createStackNavigator } from "@react-navigation/stack";
 
 const screenWidth = Dimensions.get("window").width;
 const screenHeight = Dimensions.get("window").height;
@@ -18,7 +20,6 @@ const days = ["Su", "M", "Tu", "W", "Th", "F", "Sa"];
 
 const WorkoutTrackingStack = createStackNavigator();
 
-// Separate component for each workout page
 function WorkoutPage({ route, navigation }) {
   const { workoutType } = route.params;
   return (
@@ -27,44 +28,108 @@ function WorkoutPage({ route, navigation }) {
     </View>
   );
 }
+function AddBoxButton({ onPress }) {
+  return (
+    <TouchableOpacity style={styles.floatingButton} onPress={onPress}>
+      <Feather name="plus" size={30} color="#FFF" />
+    </TouchableOpacity>
+  );
+}
 
 function WorkoutTracking({ navigation }) {
+  const [modalVisible, setModalVisible] = React.useState(false);
+  const [selectedDay, setSelectedDay] = React.useState(null);
+
   const today = new Date();
   const currentDay = today.getDay();
   const nextDays = Array(7)
     .fill()
     .map((_, i) => days[(currentDay + i) % 7]);
 
-  const workoutList = ["Push", "Pull", "Legs", "Misc"];
+  const [boxes, setBoxes] = useState([]);
+  const addBox = () => {
+    setBoxes((prevBoxes) => [...prevBoxes, `New Box ${prevBoxes.length + 1}`]);
+  };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>My Workout</Text>
-        <Image style={styles.pfp} source={require("../images/cole.jpeg")} />
-      </View>
-      <View style={styles.calendarContainer}>
-        <View style={styles.calendarLine} />
-        <View style={styles.calendar}>
-          {nextDays.map((day, index) => (
-            <View key={index} style={[styles.day, index === 0 && styles.today]}>
-              <Text style={styles.dayText}>{day}</Text>
+    <>
+      <TouchableWithoutFeedback onPress={() => setModalVisible(false)}>
+        <Modal animationType="fade" transparent={true} visible={modalVisible}>
+          <View style={styles.modalOverlay}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>{selectedDay}</Text>
+              <TouchableOpacity
+                style={styles.buttonClose}
+                onPress={() => setModalVisible(!modalVisible)}>
+                <Text style={styles.textStyle}>Hide Modal</Text>
+              </TouchableOpacity>
             </View>
-          ))}
-        </View>
-        <View style={styles.calendarLine} />
+          </View>
+        </Modal>
+      </TouchableWithoutFeedback>
+      <ScrollView style={styles.scrollView}>
+        <SafeAreaView style={styles.container}>
+          <View style={styles.header}>
+            <Image style={styles.pfp} source={require("../images/cole.jpeg")} />
+            <Text style={styles.headerText}>My Workout</Text>
+          </View>
+          <View>
+            <TouchableOpacity
+              style={styles.editButton}
+              onPress={() => {
+                console.log("Edit Button Pressed!");
+              }}>
+              <Feather name="edit" size={24} color="black" />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.calendarContainer}>
+            <View style={styles.calendar}>
+              {nextDays.map((day, index) => (
+                <TouchableOpacity
+                  key={index}
+                  style={[styles.day, index === 0 && styles.today]}
+                  onPress={() => {
+                    setSelectedDay(day);
+                    setModalVisible(true);
+                  }}>
+                  <Text style={styles.dayText}>{day}</Text>
+                </TouchableOpacity>
+              ))}
+            </View>
+          </View>
+          <View style={styles.listContainer}>
+            {boxes.length === 0 && (
+              <Text style={styles.noWorkoutsText}>
+                You have no workouts, create a new template or generate one!
+              </Text>
+            )}
+            {boxes.map((box, index) => (
+              <TouchableOpacity
+                key={index}
+                style={
+                  boxes.length % 2 == 0 || index !== boxes.length - 1
+                    ? styles.boxContainer
+                    : styles.wideBoxContainer
+                }
+                onPress={() => navigation.navigate("WorkoutPage", { workoutType: box })}>
+                <Text style={styles.boxText}>{box}</Text>
+              </TouchableOpacity>
+            ))}
+            {boxes.length >= 4 && <View style={{ height: 10 }} />}
+            <TouchableOpacity
+              style={styles.wideButton}
+              onPress={() => {
+                console.log("Wide Button Pressed!");
+              }}>
+              <Text style={styles.buttonText}>Wide Button</Text>
+            </TouchableOpacity>
+          </View>
+        </SafeAreaView>
+      </ScrollView>
+      <View style={styles.floatingButtonContainer}>
+        <AddBoxButton onPress={addBox} />
       </View>
-      <View style={styles.listContainer}>
-        {workoutList.map((workout, index) => (
-          <TouchableOpacity
-            key={index}
-            style={styles.boxContainer}
-            onPress={() => navigation.navigate("WorkoutPage", { workoutType: workout })}>
-            <Text style={styles.boxText}>{workout}</Text>
-          </TouchableOpacity>
-        ))}
-      </View>
-     </SafeAreaView>
+    </>
   );
 }
 
@@ -82,33 +147,61 @@ export default function WorkoutTrackingNavigator() {
 }
 
 const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: "#fff",
+  },
   container: {
     flex: 1,
-    paddingTop: 20,
     backgroundColor: "#fff",
   },
   header: {
     flexDirection: "row",
     alignItems: "center",
     paddingLeft: 20,
-    paddingBottom: screenHeight * 0.02,
+    justifyContent: "flex-start",
   },
   headerText: {
     fontSize: screenWidth * 0.08,
     fontWeight: "bold",
+    marginLeft: screenWidth * 0.15,
   },
   pfp: {
     width: 60,
     height: 60,
     borderRadius: 30,
-    marginLeft: screenWidth * 0.325,
   },
   calendarContainer: {
     paddingVertical: 10,
+    marginTop: 30,
+    alignItems: "center",
   },
-  calendarLine: {
-    borderBottomWidth: 1,
-    borderColor: "#000",
+  editButton: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    padding: 10,
+    elevation: 2,
+  },
+  modalOverlay: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalView: {
+    marginHorizontal: 20,
+    alignSelf: "center",
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
   },
   calendar: {
     flexDirection: "row",
@@ -116,16 +209,8 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingVertical: 10,
   },
-  day: {
-    width: screenWidth / 8,
-    height: screenWidth / 8,
-    borderRadius: screenWidth / 8,
-    backgroundColor: "#f0f0f0",
-    alignItems: "center",
-    justifyContent: "center",
-  },
   today: {
-    backgroundColor: "blue",
+    backgroundColor: "#5067FF",
   },
   dayText: {
     fontSize: screenWidth / 20,
@@ -133,24 +218,22 @@ const styles = StyleSheet.create({
   },
   listContainer: {
     width: "80%",
-    height: "80%",
-    maxWidth: "80%",
-    maxHeight: screenWidth * 0.8,
     flexDirection: "row",
     flexWrap: "wrap",
-    justifyContent: "space-around",
-    alignItems: "center",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
     alignSelf: "center",
+    marginBottom: 10,
   },
   boxContainer: {
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#f0f0f0",
-    width: "45%",
+    width: "46%",
     aspectRatio: 1,
     margin: "2%",
+    borderRadius: 12,
   },
-
   boxText: {
     fontSize: 16,
     fontWeight: "bold",
@@ -163,5 +246,53 @@ const styles = StyleSheet.create({
   workoutText: {
     fontSize: 24,
     fontWeight: "bold",
+  },
+  day: {
+    width: screenWidth / 9,
+    height: screenWidth / 9,
+    backgroundColor: "#f0f0f0",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 12,
+    margin: 5,
+  },
+  floatingButtonContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    padding: 10,
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+  },
+  floatingButton: {
+    backgroundColor: "#5067FF",
+    borderRadius: 30,
+    width: 60,
+    height: 60,
+    padding: 15,
+    elevation: 2,
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: 10,
+  },
+  wideButton: {
+    backgroundColor: "#5067FF",
+    borderRadius: 30,
+    padding: 20,
+    flex: 5,
+    elevation: 2,
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: 10,
+    marginTop: screenWidth / 2 + 10,
+  },
+  wideBoxContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f0f0f0",
+    width: "96%",
+    aspectRatio: 2,
+    margin: "2%",
+    borderRadius: 12,
   },
 });
