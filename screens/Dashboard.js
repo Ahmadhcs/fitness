93c620fa96e498cfad30ee9f27121c4dcd80e54e
Ahmadhcs <aalmesned1@gmail.com>
2023-07-10 00:01:56 +0300

    Fixing Error

--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -7,7 +7,7 @@ import {
   ScrollView,
   Pressable,
   TouchableOpacity,
-  Linking,
+  Linking
 } from "react-native";
 import React, { useLayoutEffect, useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
@@ -19,14 +19,6 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 const screenHeight = Dimensions.get("window").height;
 const screenWidth = Dimensions.get("window").width;
 
-let utubeAPIKEY = "AIzaSyCEuZka21EnXBF0yM217BQRw7mpAertXEs";
-let videoSearchArray = [
-  "pull day",
-  "Chest Day",
-  "Arms Day",
-  "Core Workout",
-  "Workout Outside",
-];
 
 let utubeAPIKEY = 'AIzaSyCEuZka21EnXBF0yM217BQRw7mpAertXEs'
 let videoSearchArray = [
@@ -90,49 +82,55 @@ let search = `https://www.googleapis.com/youtube/v3/search?key=${utubeAPIKEY}&q=
 
 let hardCodedPlaylists = [
   {
-    image: "https://i.scdn.co/image/ab67706c0000da841aeb5e4c64f71bb6dc97dd1c",
-    url: "https://open.spotify.com/playlist/6hwjHl90iQXO8JdBAbA3ky",
+    image:'https://i.scdn.co/image/ab67706c0000da841aeb5e4c64f71bb6dc97dd1c',
+    url: 'https://open.spotify.com/playlist/6hwjHl90iQXO8JdBAbA3ky'
   },
   {
-    image: "https://i.scdn.co/image/ab67706c0000da842f2eff7da6b035f1fb1ea4f7",
-    url: "https://open.spotify.com/playlist/7kfohPIiBUM4tF2B8YEn85",
+    image:'https://i.scdn.co/image/ab67706c0000da842f2eff7da6b035f1fb1ea4f7',
+    url:'https://open.spotify.com/playlist/7kfohPIiBUM4tF2B8YEn85'
   },
   {
-    image: "https://i.scdn.co/image/ab67706c0000da8444b3ae92f2d4e13759ff819d",
-    url: "https://open.spotify.com/playlist/4zbcJeMb3ERz7IXg8GrlMB",
+    image: 'https://i.scdn.co/image/ab67706c0000da8444b3ae92f2d4e13759ff819d',
+    url:'https://open.spotify.com/playlist/4zbcJeMb3ERz7IXg8GrlMB'
   },
   {
-    image: "https://i.scdn.co/image/ab67706c0000da842284acb61f89ef8ab9775c8f",
-    url: "https://open.spotify.com/playlist/0Ss1MAeS8AiwQylR6C21cj",
+    image: "https://i.scdn.co/image/ab67706c0000da842284acb61f89ef8ab9775c8f" ,
+    url: "https://open.spotify.com/playlist/0Ss1MAeS8AiwQylR6C21cj"
   },
   {
-    image: "https://i.scdn.co/image/ab67706c0000da84bea7dd2872555605bf4860c7",
-    url: "https://open.spotify.com/playlist/1BelNbncQam23oXyJBICsy",
+    image:"https://i.scdn.co/image/ab67706c0000da84bea7dd2872555605bf4860c7" , 
+    url: "https://open.spotify.com/playlist/1BelNbncQam23oXyJBICsy"
   },
   {
     image: "https://i.scdn.co/image/ab67706c0000da84a2d8311f2300d23baac12f0e",
-    url: "https://open.spotify.com/playlist/3iPLJ8T1TRjElEPOsNUISN",
+    url: "https://open.spotify.com/playlist/3iPLJ8T1TRjElEPOsNUISN"
   },
   {
     image: "https://i.scdn.co/image/ab67706c0000da84622c36e105af4aac83c34e77",
-    url: "https://open.spotify.com/playlist/7JIGfa0KkCTDxUPOQySODP",
+    url: "https://open.spotify.com/playlist/7JIGfa0KkCTDxUPOQySODP"
   },
-];
+  
+
+]
+
+
+
 
 handleClick = (link) => {
-  Linking.canOpenURL(`${link}`).then((supported) => {
-    if (supported) {
-      Linking.openURL(`${link}`);
-    } else {
-      console.log("Don't know how to open URI: " + "https://google.com");
-    }
-  });
-};
+    Linking.canOpenURL(`${link}`).then(supported => {
+      if (supported) {
+        Linking.openURL(`${link}`);
+      } else {
+        console.log("Don't know how to open URI: " + "https://google.com");
+      }
+    });
+}
 
-const chooseGymplaylist = () => {
+const chooseGymplaylist = () =>{
   const result = [];
   const copiedArr = [...hardCodedPlaylists];
 
+  
   for (let i = 0; i < 3; i++) {
     const randomIndex = Math.floor(Math.random() * copiedArr.length);
     const randomItem = copiedArr[randomIndex];
@@ -144,7 +142,6 @@ const chooseGymplaylist = () => {
 
   return result
 }
-
 
 const Dashboard = (props) => {
   const [url, setUrl] = useState('');
@@ -180,7 +177,6 @@ const Dashboard = (props) => {
         } catch (error) {
           console.error(error);
         }
-
       }
     };
 
@@ -200,7 +196,7 @@ const Dashboard = (props) => {
     const scheduleVideoUpdate = () => {
       const now = new Date();
       const targetTime = new Date(now);
-      targetTime.setHours(23, 50, 0); // Set the target time to 11:32:30 PM today
+      targetTime.setHours(23, 50, 30); // Set the target time to 11:32:30 PM today
 
       let timeDiff = targetTime.getTime() - now.getTime();
       if (timeDiff < 0) {
@@ -244,21 +240,20 @@ const Dashboard = (props) => {
   
 
 
-
-
   return (
     <SafeAreaView style={{ flex: 1 }}>
       {/* Profle Picture */}
       <View style={styles.user}>
-        <Image style={styles.pfp} source={require("../images/cole.jpeg")}></Image>
+        <Image
+          style={styles.pfp}
+          source={require("../images/cole.jpeg")}
+        ></Image>
 
         {/* Welcome Text and Date */}
         <View style={styles.innerText}>
           <Text style={styles.userText}>Welcome, Ahmad!</Text>
 
-          <Text style={styles.date}>
-            {currentDate.slice(4, 10)},{currentDate.slice(10)}
-          </Text>
+          <Text style={styles.date}>{currentDate.slice(4,10)},{currentDate.slice(10)}</Text>
         </View>
       </View>
 
@@ -275,54 +270,32 @@ const Dashboard = (props) => {
         </Text>
       </Pressable> */}
 
-      <Text
-        style={{
-          fontSize: 18,
-          fontWeight: "600",
-          paddingLeft: 20,
-          paddingTop: 10,
-          paddingBottom: 10,
-        }}>
-        Recommended Gym Playlists
-      </Text>
+      <Text style={{fontSize: 18, fontWeight: "600", paddingLeft: 20, paddingTop: 10, paddingBottom: 10}}>Recommended Gym Playlists</Text>
       <View style={styles.playListSection}>
-        <View style={{ flexDirection: "row" }}>
+        <View style={{flexDirection: 'row'}}>
           <TouchableOpacity onPress={() => handleClick(gymPlaylistArray[0].url)}>
-            <Image
-              style={styles.playlistPicture}
-              source={{ uri: gymPlaylistArray[0].image }}></Image>
+            <Image style={styles.playlistPicture}  source={{uri: gymPlaylistArray[0].image}}></Image>
           </TouchableOpacity>
 
-          <TouchableOpacity onPress={() => handleClick(gymPlaylistArray[1].url)}>
-            <Image
-              style={styles.playlistPicture}
-              source={{ uri: gymPlaylistArray[1].image }}></Image>
+          <TouchableOpacity onPress={() => handleClick(gymPlaylistArray[1].url)}> 
+          <Image style={styles.playlistPicture}  source={{uri: gymPlaylistArray[1].image}}></Image>
           </TouchableOpacity>
 
           <TouchableOpacity onPress={() => handleClick(gymPlaylistArray[2].url)}>
-            <Image
-              style={styles.playlistPicture}
-              source={{ uri: gymPlaylistArray[2].image }}></Image>
+          <Image style={styles.playlistPicture}  source={{uri: gymPlaylistArray[2].image}}></Image>
           </TouchableOpacity>
+
         </View>
+
       </View>
 
-      {/*THis is the video sextion*/}
-      <Text
-        style={{
-          fontSize: 18,
-          fontWeight: "600",
-          paddingLeft: 20,
-          paddingBottom: 5,
-          paddingTop: 5,
-        }}>
-        Fitness Videos
-      </Text>
 
-      <ScrollView
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        style={styles.videoSection}>
+
+
+      {/*THis is the video sextion*/ }
+      <Text style={{fontSize: 18, fontWeight: "600", paddingLeft: 20, paddingBottom: 5, paddingTop: 5}}>Fitness Videos</Text>
+
+      <ScrollView horizontal showsHorizontalScrollIndicator={false} style={styles.videoSection}>
         {videos.map((video) => (
         <View style={styles.videoCard}>
           <Image
@@ -334,7 +307,6 @@ const Dashboard = (props) => {
       ))}
 
       {/* <TouchableOpacity activeOpacity={1} style={styles.videoCard} onPress={() => handleClick('https://google.com')}>
-
           <Image
             source={require("../images/place.jpg")}
             style={{ width: 240, height: 120 ,top :-10, borderTopLeftRadius: 20,  borderTopRightRadius: 20}}
@@ -360,10 +332,13 @@ const Dashboard = (props) => {
                   <Text style={{paddingLeft: 10,  paddingRight: 10, fontSize: 14, fontWeight: '400', paddingTop: 8}}>Push Day with Mustafa the BUM ej fww !</Text>
 
         </View> */}
+
+
+
       </ScrollView>
 
       {/* Log Button */}
-
+    
       {/* I commented this out because I declared it in the App.js so we can
       choose which pages got a navbar
       <View style={styles.bottom}>
@@ -374,6 +349,8 @@ const Dashboard = (props) => {
 };
 
 const gymPlaylistArray = chooseGymplaylist();
+
+
 
 const styles = StyleSheet.create({
   user: {
@@ -389,18 +366,14 @@ const styles = StyleSheet.create({
     borderRadius: 60,
   },
   date: {
-    paddingLeft: 20,
+    paddingLeft: 21,
     paddingTop: 5,
-    color: "gray",
-    fontWeight: "500",
-    marginBottom: 5,
+    color: 'gray',
+    fontWeight: "500"
   },
   userText: {
     paddingLeft: 20,
     fontSize: 25,
-  },
-  innerText: {
-    // this the youtube video titles?
   },
   // Navbar styling also moved to App.js
   // bottom: {
@@ -436,36 +409,38 @@ const styles = StyleSheet.create({
     fontWeight: "700",
     marginLeft: 20,
   },
-  videoSection: {
+  videoSection:{
     paddingLeft: 8.5,
-    height: screenHeight * 0.3,
+    height: screenHeight * 0.3
+
   },
-  videoCard: {
+  videoCard:{
     borderRadius: 20,
     marginHorizontal: 7.5,
-    backgroundColor: "white",
+    backgroundColor: 'white',
     paddingTop: 10,
     width: 240,
     height: screenHeight * 0.225,
     marginTop: 10,
     borderTopStartRadius: 300,
     borderTopEndRadius: 300,
-    shadowOffset: { width: -5, height: 3 },
-    shadowColor: "gray",
+    shadowOffset: {width: -5, height: 3},
+    shadowColor: 'gray',
     shadowOpacity: 0.5,
-    shadowRadius: 3,
+    shadowRadius: 3
   },
-  playListSection: {
+  playListSection:{
     height: screenHeight * 0.15,
     paddingLeft: 15,
     paddingTop: 5,
   },
-  playlistPicture: {
+  playlistPicture:{
     width: screenWidth * 0.275,
     height: screenWidth * 0.275,
     borderRadius: 25,
-    marginHorizontal: 7.5,
-  },
+    marginHorizontal: 7.5
+
+  }
 });
 
 export default (chooseGymplaylist) => <Dashboard gymPlaylistArray={gymPlaylistArray} />;
