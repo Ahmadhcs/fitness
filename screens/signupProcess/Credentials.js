b2b5576970b5605e4c765260862ac41fc917d194
Ahmadhcs <aalmesned1@gmail.com>
2023-07-11 01:30:08 +0300

    ignore

--- screens/signupProcess/Credentials.js
+++ screens/signupProcess/Credentials.js
@@ -18,7 +18,6 @@ const Credentials = () => {
   const navigation = useNavigation();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
