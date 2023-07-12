eac8d5a88811b3e5d976b57a5e026838c3c1693e
Mustafa <mustafataibah@gmail.com>
2023-07-12 05:36:07 -0400

    WHY AM I EVEN TRING TO DO THIS

--- components/ExerciseCard.js
+++ components/ExerciseCard.js
@@ -1,24 +1,98 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
+import { PanGestureHandler } from "react-native-gesture-handler";
+import Animated, {
+  runOnJS,
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
+import { Feather } from "@expo/vector-icons";
 
-const ExerciseCard = ({ name }) => {
+const LIST_ITEM_HEIGHT = 50;
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+const TRANSLATE_X_THRESHOLD = -SCREEN_WIDTH * 0.3;
+
+function ExerciseCard({ data, onDelete }) {
+  const translateX = useSharedValue(0);
+
+  const panGesture = useAnimatedGestureHandler({
+    onActive: (event) => {
+      translateX.value = event.translationX;
+    },
+    onEnd: () => {
+      const shouldBeDismissed = translateX.value < TRANSLATE_X_THRESHOLD;
+      if (shouldBeDismissed) {
+        translateX.value = withSpring(-SCREEN_WIDTH);
+        runOnJS(onDelete)();
+      } else {
+        translateX.value = withSpring(0);
+      }
+    },
+  });
+
+  const rStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateX: translateX.value,
+      },
+    ],
+  }));
+
+  const rIconContainerStyle = useAnimatedStyle(() => {
+    return {
+      opacity: translateX.value < TRANSLATE_X_THRESHOLD ? 1 : 0,
+    };
+  });
+
   return (
-    <View style={styles.card}>
-      <Text style={styles.text}>{name}</Text>
-    </View>
+    <Animated.View style={styles.taskContainer}>
+      <Animated.View style={[styles.iconContainer, rIconContainerStyle]}>
+        <Feather name={"trash-2"} size={LIST_ITEM_HEIGHT * 0.4} color={"red"} />
+      </Animated.View>
+      <PanGestureHandler onGestureEvent={panGesture}>
+        <Animated.View style={[styles.task, rStyle]}>
+          <Text style={styles.taskTitle}>{data}</Text>
+        </Animated.View>
+      </PanGestureHandler>
+    </Animated.View>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  card: {
-    marginVertical: 10,
-    padding: 15,
-    backgroundColor: "#FFF",
-    borderRadius: 5,
-    elevation: 1,
+  taskContainer: {
+    width: "100%",
+    alignItems: "center",
+    flexDirection: "row",
   },
-  text: {
+  task: {
+    width: "100%",
+    height: LIST_ITEM_HEIGHT,
+    justifyContent: "center",
+    paddingLeft: 20,
+    backgroundColor: "white",
+    borderRadius: 10,
+    // Shadow for iOS
+    shadowOpacity: 0.08,
+    shadowOffset: {
+      width: 0,
+      height: 20,
+    },
+    shadowRadius: 10,
+    // Shadow for Android
+    elevation: 5,
+  },
+  taskTitle: {
     fontSize: 16,
+  },
+  iconContainer: {
+    height: LIST_ITEM_HEIGHT,
+    width: LIST_ITEM_HEIGHT,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    right: 0,
   },
 });
 
